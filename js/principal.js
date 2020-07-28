314663f53f515762b7ce43804660a811e209f970
Aparecida <aparecida@MacBook-Pro-de-Fix.local>
2020-07-28 13:40:45 -0300

    Terceira alteração do projeto de javaScript

--- js/principal.js
+++ js/principal.js
@@ -3,3 +3,18 @@ console.log("Fui carregado de um arquivo externo.");
 var titulo = document.querySelector(".titulo");
 
 titulo.textContent = "Aparecida Nutricionista";
+
+var paciente = document.querySelector("#primeiro-paciente");
+
+var tdpeso = paciente.querySelector(".info-peso");
+var peso = tdpeso.textContent;
+
+var tdaltura = paciente.querySelector(".info-altura");
+var altura = tdaltura.textContent;
+
+var imc = peso/(altura * altura);
+
+var tdimc = paciente.querySelector(".info-imc");
+
+tdimc.textContent = imc;
+
