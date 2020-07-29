122c49cea5026cb98325a7a85cbcd84d58075cf5
Aparecida <aparecida@MacBook-Pro-de-Fix.local>
2020-07-29 11:48:40 -0300

    Quinto commit do projeto de javaScript

---
+++ js/calcula-imc.js
@@ -0,0 +1,55 @@
+console.log("Fui carregado de um arquivo externo.");
+
+var titulo = document.querySelector(".titulo");
+
+titulo.textContent = "Aparecida Nutricionista";
+
+var pacientes = document.querySelectorAll(".paciente");
+
+
+
+for (var i=0; i < pacientes.length; i++){
+    console.log(pacientes[i]);
+
+
+    var paciente = pacientes[i];
+
+
+    var tdpeso = paciente.querySelector(".info-peso");
+    var peso = tdpeso.textContent;
+
+    var tdaltura = paciente.querySelector(".info-altura");
+    var altura = tdaltura.textContent;
+
+    var tdimc = paciente.querySelector(".info-imc");
+
+    var pesoValido = true;
+    var alturaValida = true;
+
+    if(peso <= 0 || peso >= 1000) {
+        console.log("Peso inválido!");
+        pesoValido = false;
+        tdimc.textContent = "Peso Inválido!";
+        paciente.style.backgroundColor = "red";
+        paciente.classList.add("paciente-invalido");
+    }
+    if(altura <= 0 || altura >= 3.00){
+        console.log("Altura Inválida!");
+        alturaValida = false;
+        tdimc.textContent = "Altura Inválida";
+        paciente.classList.add("paciente-invalido");
+    }
+    if(pesoValido && alturaValida){
+        var imc = peso/(altura * altura);
+        tdimc.textContent = imc.toFixed(2);
+    }
+
+}
+
+
+
+
+
+
+
+
---
+++ js/form.js
@@ -0,0 +1,40 @@
+
+var botao = document.querySelector("#adicionar-paciente"); /*pegando o botao do html */
+
+botao.addEventListener("click", adicionar); /* Criando o evento de click */
+
+/* Inicio da Função do evento de click */
+
+function adicionar(event) {
+    event.preventDefault();
+
+    var paciente = document.querySelector(".paciente-adicionado");/* Pegando o form com os inputs */
+
+    var nome = paciente.nome.value; /* Passando o valor dos inputs para uma variável */
+    var peso = paciente.peso.value; /* Passando o valor dos inputs para uma variável */
+    var altura = paciente.altura.value; /* Passando o valor dos inputs para uma variável */
+    var gordura = paciente.gordura.value; /* Passando o valor dos inputs para uma variável */
+
+    var pacienteTr = document.createElement("tr"); /* Criando uma TR atraves do JS */
+
+    var nomeTd = document.createElement("td"); /* Criando um TD atraves do JS */
+    var pesoTd = document.createElement("td"); /* Criando um TD atraves do JS */
+    var alturaTd = document.createElement("td"); /* Criando um TD atraves do JS */
+    var gorduraTd = document.createElement("td"); /* Criando um TD atraves do JS */
+    var imcTd = document.createElement("td"); /* Criando um TD atraves do JS */
+
+    nomeTd.textContent = nome; /* Passando apenas os valores das TD */
+    pesoTd.textContent = peso;
+    alturaTd.textContent = altura;
+    gorduraTd.textContent = gordura;
+
+    pacienteTr.appendChild(nomeTd);/* passando as td como filhas da tr */
+    pacienteTr.appendChild(pesoTd);
+    pacienteTr.appendChild(alturaTd);
+    pacienteTr.appendChild(gorduraTd);
+
+    var tabela = document.querySelector("#tabela-pacientes"); /* Pegando o corpo da tabela */
+
+    tabela.appendChild(pacienteTr);
+
+}
